9dd7cb696ee366cb924b33a3b25b082df256eb71
Lahiru Wimalasena <lwimala@emory.edu>
2018-12-19 13:25:13 -0500

    Updated run_lfadslite.py with default params for loss scaling/adam optimizer presets

--- run_lfadslite.py
+++ run_lfadslite.py
@@ -98,6 +98,12 @@ PRIOR_AR_PROCESS_VAR = 0.1
 DO_TRAIN_PRIOR_AR_ATAU = True
 DO_TRAIN_PRIOR_AR_NVAR = True
 
+# params for loss scaling/ ADAM optimizer
+LOSS_SCALE = 1e4
+ADAM_EPSILON = 1e-8
+ADAM_BETA1 = 0.9
+ADAM_BETA2 = 0.999
+
 flags = tf.app.flags
 flags.DEFINE_string("kind", "train",
                     "Type of model to build {train, \
@@ -371,6 +377,17 @@ flags.DEFINE_float("kl_ic_weight", KL_IC_WEIGHT,
 flags.DEFINE_float("kl_co_weight", KL_CO_WEIGHT,
                    "Strength of KL weight on controller output KL penalty.")
 
+# LOSS SCALING/ADAM OPTIMIZER PRESETS
+flags.DEFINE_float("loss_scale", LOSS_SCALE,
+                   "Scaling of loss.")
+flags.DEFINE_float("adam_epsilon", ADAM_EPSILON,
+                   "Epsilon parameter of ADAM optimizer.")
+flags.DEFINE_float("adam_beta1", ADAM_BETA1,
+                   "Beta1 parameter of ADAM optimizer.")
+flags.DEFINE_float("adam_beta2", ADAM_BETA2,
+                   "Beta2 parameter of ADAM optimizer.")
+
+
 FLAGS = flags.FLAGS
 
 
@@ -558,6 +575,12 @@ def build_hyperparameter_dict(flags):
   d['l2_start_step'] = flags.l2_start_step
   d['l2_increase_steps'] = flags.l2_increase_steps
 
+  # Loss scaling/Adam Optimizer Presets
+  d['loss_scale'] = flags.loss_scale
+  d['adam_epsilon'] = flags.adam_epsilon
+  d['beta1'] = flags.adam_beta1
+  d['beta2'] = flags.adam_beta2
+  
   return d
 
 class hps_dict_to_obj(dict):
