7d3a39705ce9b24b2f2a4aaaebc18196f968ddd1
Lahiru Wimalasena <lwimala@emory.edu>
2021-05-10 17:35:17 -0400

    fix name in setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 # conda install -c anaconda cudatoolkit
 
 setup(
-    name="lfadslite",
+    name="lfads_tf1",
     version="0.1.0",
     packages=find_packages(),
     install_requires=[
